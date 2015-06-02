b71e94071fe8e9dc1600632916c19cbf89853bbd
Daniel Sorichetti <me@dancodes.com.ar>
2015-06-02 17:03:39 -0300

    Arreglo el tilde que rompia todo el build

--- src/razas/interfaces/Construccion.java
+++ src/razas/interfaces/Construccion.java
@@ -7,7 +7,7 @@ public interface Construccion {
 	public int tiempoDeConstruccion();
 	
 	/*public ? habilitaConstruir();
-	* El método tiene que devolver algo que diga que puede construir o 
+	* El mÃ©todo tiene que devolver algo que diga que puede construir o 
 	* si no cuando se quiera crear la unidad se puede revisar si es posible crearla
 	* revisando las unidades que posee el jugado y no hacemos un metodo.
 	*/
